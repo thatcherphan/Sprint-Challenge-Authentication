eb3220deb1b7d31264e9a5d1cb7c5a12e4845571
Th5tch3r <amber.phan27@gmail.com>
2020-02-21 23:32:06 -0500

    middleware done, now moving on to testing

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,21 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({message: "Wrong token, or token has expired, please log in again"})
+      } else {
+        req.user = {
+          username: decodedToken.username
+        };
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({message: "you shall not pass"})
+  }
 };
